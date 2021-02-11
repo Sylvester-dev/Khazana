aef4d2f8eacf0996deb2b82fe69609e3a62c5ee4
VineetMaker <vk.vempire@gmail.com>
2021-02-11 16:51:13 +0530

    nft

--- tickets/src/screens/Home.js
+++ tickets/src/screens/Home.js
@@ -29,40 +29,40 @@ function Home() {
     <section id="sec">
      <div id="dout">
          <div id="d1">
-           <h1>We make all this possibleeeeeeeeeeeeeeeeeeeeeeeeeeee</h1>
+           <h1>FAST</h1>
           </div>
           <div id="d2">
-             <img id="pich" src="https://s3.amazonaws.com/hedera-com/_w1000/hh-cryptocurrency-meta-image.jpg"></img>
+             <img id="pich" src="https://s3.us-east-1.amazonaws.com/hedera-com/homepage-value-prop-1.png"></img>
           </div>
      </div>
     </section>
     <section id="sec">
      <div id="dout">
           <div id="d1">
-             <img id="pich" src="https://s3.amazonaws.com/hedera-com/_w1000/hh-cryptocurrency-meta-image.jpg"></img>
+             <img id="pich" src="https://s3.us-east-1.amazonaws.com/hedera-com/homepage-value-prop-3.png"></img>
           </div>
          <div id="d2">
-           <h1>We make all this possibleeeeeeeeeeeeeeeeeeeeeeeeeeee</h1>
+           <h1>SECURE</h1>
          </div>
      </div>
     </section>
     <section id="sec">
      <div id="dout">
          <div id="d2">
-           <h1>We make all this possibleeeeeeeeeeeeeeeeeeeeeeeeeeee</h1>
+           <h1>FAIR</h1>
           </div>
           <div id="d1">
-             <img id="pich" src="https://s3.amazonaws.com/hedera-com/_w1000/hh-cryptocurrency-meta-image.jpg"></img>
+             <img id="pich" src="https://s3.us-east-1.amazonaws.com/hedera-com/homepage-value-prop-2.png"></img>
           </div>
      </div>
     </section>
     <section id="sec">
      <div id="dout">
           <div id="d2">
-             <img id="pich" src="https://s3.amazonaws.com/hedera-com/_w1000/hh-cryptocurrency-meta-image.jpg"></img>
+             <img id="pich" src="https://s3.us-east-1.amazonaws.com/hedera-com/homepage-value-prop-4.png"></img>
           </div>
          <div id="d1">
-           <h1>We make all this possibleeeeeeeeeeeeeeeeeeeeeeeeeeee</h1>
+           <h1>STABLE</h1>
           </div>
      </div>
     </section>
