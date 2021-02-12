d5ba8a0a9db108efb0559472fceab35d15f42d97
VineetMaker <vk.vempire@gmail.com>
2021-02-13 02:50:27 +0530

    changes

--- tickets/src/screens/Home.js
+++ tickets/src/screens/Home.js
@@ -35,7 +35,7 @@ function Home() {
           <div id="d2">
             <img
               id="pich"
-              src="https://s3.us-east-1.amazonaws.com/hedera-com/homepage-value-prop-1.png"
+              src="https://www.usa.philips.com/c-dam/b2bhc/master/education-resources/copd-insider/common/rocket-animated.gif"
             ></img>
           </div>
         </div>
@@ -45,7 +45,7 @@ function Home() {
           <div id="d1">
             <img
               id="pich"
-              src="https://s3.us-east-1.amazonaws.com/hedera-com/homepage-value-prop-3.png"
+              src="http://www.cirrus-connect.com/wp-content/uploads/2017/05/secure.gif"
             ></img>
           </div>
           <div id="d2">
@@ -54,6 +54,7 @@ function Home() {
         </div>
       </section>
       <section id="sec">
+      
         <div id="dout">
           <div id="d2">
             <h1>FAIR</h1>
@@ -78,7 +79,111 @@ function Home() {
             <h1>STABLE</h1>
           </div>
         </div>
-      </section>
+        </section>
+        <section id="sec">
+        <div id="dout">
+         <div id="d1">
+            <h1>Dynamically changing QR Code</h1>
+          </div>
+          <div id="d2">
+            <img
+              id="pich"
+              src="https://i.redd.it/y3bpgdy80fry.gif"
+            ></img>
+          </div>
+        </div>
+        </section>
+        <section id="sec">
+        <div id="dout">
+          <div id="d2">
+            <img
+              id="pich"
+              src="https://gifimage.net/wp-content/uploads/2018/06/ticket-gif-5.gif"
+            ></img>
+          </div>
+          <div id="d1">
+            <h1>Smart Tickets not being Smart Enough?</h1>
+          </div>
+
+        </div>
+        </section>
+        <section id="sec">
+        <div id="dout">
+        <div id="d1">
+            <h1>Help others in need by donation or rasing Fund</h1>
+          </div>
+          <div id="d2">
+            <img
+              id="pich"
+              src="https://media.giphy.com/media/l0CLV6Yk17s2ZIqsw/giphy.gif"
+            ></img>
+          </div>
+
+        </div>
+        </section>
+       
+        <section id="sec">
+        <div id="dout">
+          <div id="d2">
+            <img
+              id="pich"
+              src="https://res.cloudinary.com/sagacity/image/upload/c_crop,h_1311,w_1362,x_0,y_0/c_limit,f_auto,fl_lossy,q_80,w_640/Real_Estate_caw7me.gif"
+            ></img>
+          </div>
+          <div id="d1">
+            <h1>Sell and Buy Property</h1>
+          </div>
+
+        </div>
+        </section>
+        <section id="sec">
+        <div id="dout">
+        <div id="d1">
+            <h1>Save TIME & MONEY</h1>
+          </div>
+          <div id="d2">
+            <img
+              id="pich"
+              src="https://bestanimations.com/Money/time-is-money-animated-gif.gif"
+            ></img>
+          </div>
+
+        </div>
+        </section>
+        
+        
+        <section id="sec">
+        <div id="dout">
+          <div id="d2">
+            <img
+              id="pich"
+              src="https://media1.tenor.com/images/08545121f55f3e90d8155b879e97d84d/tenor.gif?itemid=5145613"
+            ></img>
+          </div>
+          <div id="d1">
+            <h1>Get Real value for Your Talent</h1>
+          </div>
+
+        </div>
+        </section>
+
+        <section id="sec">
+        <div id="dout">
+          <div id="d1">
+            <h1>DO More...</h1>
+          </div>
+          <div id="d2">
+            <img
+              id="pich"
+              src="https://cdn.dribbble.com/users/744367/screenshots/2684910/eddykoek_36days_num6_01.gif"
+            ></img>
+          </div>
+
+
+        </div>
+        </section>
+        
+      
     </>
   );
 }
